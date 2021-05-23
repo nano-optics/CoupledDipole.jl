fe425a9d4f48e2dc92c0c64af63f3392c0bd7c42
baptiste auguie <baptiste.auguie@gmail.com>
2021-05-23 18:08:02 +1200

    tseting makie

---
+++ dev/Visual.jl
@@ -0,0 +1,89 @@
+function visualise(cl)
+
+        s = []
+        for i ∈ 1:length(cl.positions)
+                x = cl.positions[i][1]
+                y = cl.positions[i][2]
+                z = cl.positions[i][3]
+
+                a = cl.sizes[i][1]
+                b = cl.sizes[i][2]
+                c = cl.sizes[i][3]
+
+                α = cl.angles[i][1]
+                β = cl.angles[i][2]
+                γ = cl.angles[i][3]
+                push!(s,
+                        "var sphere$i = createSphere(radius, segments);
+                   sphere$i.position.x = $x;
+                   sphere$i.position.y = $y;
+                   sphere$i.position.z = $z;
+                   sphere$i.scale.set($a,$b,$c);
+                   sphere$i.rotation.x = $α;
+                   sphere$i.rotation.y = $β;
+                   sphere$i.rotation.z = $γ;
+                   scene.add(sphere$i);\n",
+                )
+        end
+
+        s
+
+
+end
+
+cl =cluster_helix(10, 20, 20, 40, 50, 300)
+
+
+visualise(cl)
+
+
+function euler_to_axisangle(e)
+        M = euler_passive(e...)
+        θ = acos((M[1, 1] + M[2, 2] + M[3, 3] - 1) / 2)
+        e1 = (M[3, 2] - M[2, 3]) / (2sin(θ))
+        e2 = (M[1, 3] - M[3, 1]) / (2sin(θ))
+        e3 = (M[2, 1] - M[1, 2]) / (2sin(θ))
+
+        θ .* Vec3f0(e1, e2, e3)
+end
+
+# euler_to_axisangle([0,pi/2,0])
+
+cl = cluster_dimer(10, 1, 2, 3)
+
+meshscatter(
+    Point3f0.(cl.positions),
+    marker =sphere,
+    markersize = 1.0.*Vec3f0.(cl.sizes),
+    #markersize = [Vec3f0(10,10,20) for i in 1:length(cl.positions)],
+    rotation = euler_to_axisangle.(cl.angles),
+    color=:red
+)
+
+
+
+
+
+
+
+using Makie
+meshscatter(
+    rand(Point3f0, 10),
+    marker =sphere,
+    markersize = [Vec3f0(0.1,0.1,0.2) for i in 1:10],
+    rotation = [Vec3f0(0,  0,pi/2) for i in 1:10],
+    color=:red
+)
+
+
+
+
+
+using Makie
+using GLMakie
+p1 = Point3(0.0, 0.0, 0.0)
+p2 = Point3(0.0, 1.0, 0.0)
+p3 = Point3(0.0, 1.0, 1.0)
+p4 = Point3(0.0, 0.0, 1.0)
+
+sc = mesh([p1, p2, p3], color = :blue, shading = false)
---
+++ dev/test_krylov.jl
@@ -0,0 +1,89 @@
+
+
+
+push!(LOAD_PATH, expanduser( "~/Documents/nano-optics/CoupledDipole.jl/"))
+using Revise
+using CoupledDipole
+using IterativeSolvers
+
+
+
+using LinearAlgebra
+using StaticArrays
+using FastGaussQuadrature
+using DataFrames
+using VegaLite
+# using Gadfly
+
+## variables
+
+N_dip = 1000
+N_inc = 2
+N_lam = 2
+N_sca = 2
+
+wavelength = [400]
+medium = Dict([("Au", CoupledDipole.epsilon_Au), ("medium", x -> 1.33)])
+
+mat = Material(wavelength, medium)
+
+cl = cluster_helix(N_dip, 20, 20, 30, 50, 300)
+
+## polarizability
+
+ii=1;
+
+λ = mat.wavelengths[ii]
+n_medium = mat.media["medium"](λ)
+kn = n_medium * 2π / λ
+
+ε_name = cl.material # e.g. "Au" to refer to epsilon_Au in mat Dict
+ε = mat.media[ε_name](λ)
+Alpha = alpha_spheroids(λ, ε, n_medium^2, cl.sizes)
+
+ParticleRotations = map(euler_passive, cl.angles)
+AlphaBlocks = map((R, A) -> R' * diagm(A) * R, ParticleRotations, Alpha)
+
+## propagator
+
+F = Matrix{Complex{Float64}}(I, 3*N_dip, 3*N_dip)
+propagator_freespace_labframe!(F,kn,cl.positions,AlphaBlocks)
+
+## incidence
+
+Ejones = [SVector{2}(1.0, 0.0), SVector{2}(0.0, 1.0)]
+
+angles = cubature_sphere(30)
+N_inc = length(angles.nodes)
+IncidenceRotations = map(euler_active, angles.nodes)
+
+
+Evec1 = SVector(Ejones[1][1], Ejones[1][2], 0) # 3-vector
+Evec2 = SVector(Ejones[2][1], Ejones[2][2], 0) # 3-vector
+
+Ein = Array{Complex{Float64}}(undef, (3*N_dip, 2N_inc))
+incident_field!(Ein, Ejones, kn, cl.positions, IncidenceRotations)
+
+E = similar(Ein)
+@time E = F\Ein
+
+Eit = copy(Ein[:,1])
+@time idrs!(Eit, F, Ein[:,1])
+
+
+test_direct = function(n)
+    E = similar(Ein)
+    t = @time x= F\Ein
+    t
+end
+
+test_iterative = function(n)
+for i ∈ 1:N_inc
+    Eit = copy(Ein[:,i])
+     idrs!(Eit, F, Ein[:,i], abstol=1e-3)
+end
+
+end
+
+test_direct(1)
+@time test_iterative(1)
